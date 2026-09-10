e875aef1be8e3771d0dcbe613737cca6cbda246a
Mike <mikeward5@hotmail.co.uk>
2026-09-10 16:22:08 +0100

    repo-branch commit added header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-console.log("Webpack is working!");
-console.log("second test");
-import "./index.css";
+import './index.css';
+
+const container = document.querySelector('#content'); //select the Content Div
+const header = document.createElement('header'); //create header container
+
+const links = ['Home', 'Menu', 'Contact Us'] //list of Links
+links.forEach((item)=> { //loop through the list create the List items and append to the NAV ul container
+    const button = document.createElement('button')
+    button.textContent = item
+    header.appendChild(button)
+});
+
+container.appendChild(header); //append the header to the Content Div
