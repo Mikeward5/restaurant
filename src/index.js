b0171b3f570e5b9574d6c88a9c870d685a2edbe0
Mike <mikeward5@hotmail.co.uk>
2026-09-10 16:53:38 +0100

    starting pont for switching divs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-
+const body = document.querySelector('body');
 const container = document.querySelector('#content'); //select the Content Div
 const header = document.createElement('header'); //create header container
 
@@ -7,7 +7,13 @@ const links = ['Home', 'Menu', 'Contact Us'] //list of Links
 links.forEach((item)=> { //loop through the list create the List items and append to the NAV ul container
     const button = document.createElement('button')
     button.textContent = item
+    button.addEventListener('click', () => alert(`You clicked ${item}`))
     header.appendChild(button)
 });
 
-container.appendChild(header); //append the header to the Content Div
+const content = document.createElement('div');
+content.textContent = 'hello';
+container.appendChild(content)
+
+body.appendChild(header); //append the header to the Content Div
+body.appendChild(container);
